0e56e207707c177104192fcc1bd1b133abe46fe0
MercyNsoh <126034897+MercyNsoh@users.noreply.github.com>
2023-04-09 16:56:28 -0500

    Update jenkins_lib.groovy

--- vars/jenkins_lib.groovy
+++ vars/jenkins_lib.groovy
@@ -1,3 +1,12 @@
+def welcome(name){
+  echo "hello ${name}, you just used a shared library"
+}
+
+def newfile(file_name){
+  echo "creating file called ${file_name}"
+  sh "touch ${file_name}"
+}
+
 def call(){
-  echo "you just used a shared library"
+  echo "You had to use just the name of the file here"
 }
