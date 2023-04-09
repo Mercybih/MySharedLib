6bbc96fbe060761897882660c30f466127b4f488
MercyNsoh <126034897+MercyNsoh@users.noreply.github.com>
2023-04-09 17:12:58 -0500

    Update jenkins_lib.groovy

--- vars/jenkins_lib.groovy
+++ vars/jenkins_lib.groovy
@@ -1,5 +1,5 @@
 def welcome(name, devops_class){
-  echo "Hello ${name} from Class ${devop_class}, you just used a shared library"
+  echo "Hello ${name} from Class ${devops_class}, you just used a shared library"
 }
 
 def newfile(file_name){
